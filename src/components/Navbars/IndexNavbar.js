007ce1617bb8e79ad37a061f845f86c6a82a484a
Maytheu <maytheu98@gmail.com>
2021-12-10 10:56:00 +0100

    initial commit

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -9,9 +9,12 @@ export default function Navbar(props) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+      {/* <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"> */}
+      <nav className="top-0 fixed z-50 w-full flex items-center justify-evenly px-2 py-3 navbar-expand-lg bg-white shadow">
+        {/* <div className="container px-4 mx-auto flex flex-wrap items-center justify-between"> */}
+        <div className="container px-4 mx-auto flex items-center justify-evenly">
+          {/* <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"> */}
+          <div className="w-full relative flex justify-evenly lg:w-auto lg:static lg:block">
             <Link
               to="/"
               className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
